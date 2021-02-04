06760ba7ff59e8575d8e293dea5d431d15ebf764
fmentesana <minitocasa1@gmail.com>
2021-02-04 00:55:44 -0300

    commit1

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EmailValidator } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
+  name: string;
+  email: string;
+  prov: string;
+  codArea: number;
+  tel: number;
+  message: string;
+
+
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  submitForm() {
+    alert("Su consulta fue enviada. Nos contactaremos a la brevedad")
   }
 
 }
